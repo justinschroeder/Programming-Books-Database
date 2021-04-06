e7a16f9bacbdfe6ea8eac534babc066d85fd0c1f
Justin Schroeder <justin.schroeder3@gmail.com>
2021-04-06 17:39:15 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# import models
+# main menu - add, search, analysis, exit, view
+# add books to db
+# edit books in db
+# delete books from db
+# search books in db
+# data cleaning functions
+# loop runs program
