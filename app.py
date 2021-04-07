af291ae3da1e32659e6e10041a540d08b071a53a
Justin Schroeder <justin.schroeder3@gmail.com>
2021-04-07 17:48:35 -0500

    added database model

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import models
+from models import (Base, session, Book, engine)
 # main menu - add, search, analysis, exit, view
 # add books to db
 # edit books in db
@@ -6,3 +6,7 @@
 # search books in db
 # data cleaning functions
 # loop runs program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
