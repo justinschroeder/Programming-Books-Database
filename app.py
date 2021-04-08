3b04cb9316adce352944f664ab9966a8cb778d7d
Justin Schroeder <justin.schroeder3@gmail.com>
2021-04-07 20:52:10 -0500

    able to search specific books

--- app.py
+++ app.py
@@ -66,6 +66,28 @@ def clean_price(price_str):
         return int(price_float * 100)
 
 
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        print('''
+        \n*******ID ERROR*******
+        \rID should be a number.
+        \rPlease try again.
+        \r**********************''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            print(f'''
+            \n*******ID ERROR*******
+            \rOptions: {options}
+            \rPlease try again.
+            \r**********************''')
+            return
+
+
 def add_csv():
     with open('suggested_books.csv') as csv_file:
         data = csv.reader(csv_file)
@@ -111,8 +133,23 @@ def app():
             input('\nPress ENTER to return to main menu.')
             pass
         elif choice == '3':
-            # search books
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                \nID Options: {id_options}
+                \rBook ID:  ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+            \n{the_book.title} by {the_book.author}
+            \rPublished: {the_book.published_date}
+            \rprice: ${the_book.price / 100}''')
+            input('\nPress ENTER to return to main menu.')
         elif choice == '4':
             # analysis
             pass
